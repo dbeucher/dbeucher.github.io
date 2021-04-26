208cdab53cf559b16db797f8d49c9099a05f056b
Damien B <blakpunter+git@gmail.com>
2021-04-26 22:16:22 +0200

    Ajout des 30kms

--- asset/js/app.js
+++ asset/js/app.js
@@ -3,6 +3,7 @@ let map
 let markers
 let tiles
 const distance = 10000;
+const adresseInput = document.getElementById('adresse');
 
 const hasGeolocationSupport = () => {
     if (!navigator.geolocation) {
@@ -49,6 +50,11 @@ const positionUpdate = () => {
         radius: distance
     }).addTo(markers)
 
+    let bigCircle = L.circle([position.latitude, position.longitude], {
+        color: 'yellow',
+        radius: 30000
+    }).addTo(markers)
+
     map.fitBounds(circle.getBounds({
         padding: [5, 5]
     }))
@@ -66,21 +72,48 @@ const findAddress = () => {
     fetch(`https://api-adresse.data.gouv.fr/reverse/?lon=${position.longitude}&lat=${position.latitude}`)
         .then((response) => response.json())
         .then(response => {
-            document.getElementById('adresse').value = response.features[0].properties.label
+            adresseInput.value = response.features[0].properties.label
         })
         .catch(err => handleMessage(err))
 }
 
-document.getElementById('adresse').addEventListener('change', function (e) {
-    const adresseName = e.target.value
-    fetch(`https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(adresseName)}`)
-        .then(response => response.json())
-        .then(response => {
-            position.latitude = response.features[0].geometry.coordinates[1]
-            position.longitude = response.features[0].geometry.coordinates[0]
-            positionUpdate()
-        })
-        .catch(err => console.error(err))
+
+adresseInput.addEventListener('keyup', function (e) {
+    const adresseValue = adresseInput.value.trim();
+    if (adresseValue.length > 3) {
+        fetch(`https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(adresseValue)}&limit=5`)
+            .then(response => response.json())
+            .then(response => {
+                if (response.features.length > 1) {
+                    const propositionsListeElement = document.getElementById('propositions')
+                    propositionsListeElement.innerHTML = ''
+                    propositionsListeElement.classList.add('touchable');
+                    response.features.forEach((feature) => {
+                        const proposition = document.createElement('li')
+                        proposition.classList.add('proposition')
+                        proposition.innerText = feature.properties.label
+                        proposition.setAttribute('data-latitude', feature.geometry.coordinates[1])
+                        proposition.setAttribute('data-longitude', feature.geometry.coordinates[0])
+                        propositionsListeElement.append(proposition);
+                        proposition.addEventListener('click', function(e) {
+                            document.getElementById('adresse').value = e.target.innerText;
+                            position.latitude = e.target.getAttribute('data-latitude')
+                            position.longitude = e.target.getAttribute('data-longitude')
+                            propositionsListeElement.innerHTML = '';
+                            positionUpdate();
+                         })
+                    })
+
+                } else {
+                    position.latitude = response.features[0].geometry.coordinates[1]
+                    position.longitude = response.features[0].geometry.coordinates[0]
+                    positionUpdate()
+                }
+            })
+            .catch(err => console.error(err))
+    } else {
+        document.getElementById('propositions').innerHTML = '';
+    }
 })
 
 const handleMessage = (msg, type = 'error') => {
